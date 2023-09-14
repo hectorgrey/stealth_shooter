ec0ecf88c91997b139737ec5a07db34bc9276cd8
Chris Parker <hectorgrey223@protonmail.com>
2023-09-14 06:02:53 +0100

    Added movement and stance components

--- src/characters.rs
+++ src/characters.rs
@@ -3,3 +3,23 @@ use bevy::prelude::*;
 #[derive(Component)]
 struct Player;
 
+#[derive(Default, Clone, Copy, Debug, Hash, PartialEq, Eq, States, Component)]
+enum MovementState {
+    #[default]
+    Idle,
+    SlowWalk,
+    Walk,
+    Run,
+    Sprint,
+    Crawl,
+    Climb,
+    Jump,
+}
+
+#[derive(Default, Clone, Copy, Debug, Hash, PartialEq, Eq, States, Component)]
+enum StanceState {
+    #[default]
+    Standing,
+    Crouched,
+    Prone,
+}
