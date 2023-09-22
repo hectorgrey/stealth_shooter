901fb1cf99e80ab71f8965fff3333b491266c492
hectorgrey <parker321@gmail.com>
2023-09-22 16:24:01 +0100

    made a start on creating the player

--- src/characters.rs
+++ src/characters.rs
@@ -29,11 +29,22 @@ enum StanceState {
 pub fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
     let skybox = asset_server.load("blank_map_skybox.png");
     commands.spawn((
+        Player,
         Camera3dBundle {
             transform: Transform::from_xyz(0., 1.75, 0.),
             ..Default::default()
         },
         Skybox(skybox.clone()),
+        KinematicCharacterController {
+            translation: Some(Vec3 {
+                x: 0.,
+                y: 0.,
+                z: 0.,
+            }),
+            custom_mass: Some(80.),
+            apply_impulse_to_dynamic_bodies: true,
+            ..Default::default()
+        },
     ));
     commands.insert_resource(SkyboxTexture {
         texture_handle: skybox,
